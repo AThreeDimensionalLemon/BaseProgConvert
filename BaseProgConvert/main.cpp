65d951da395fd851bba33523a38d0b93546af5ba
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-02-01 15:21:59 -0800

    Changed values in `main` to better demonstrate the program's capabilities

--- BaseProgConvert/main.cpp
+++ BaseProgConvert/main.cpp
@@ -14,19 +14,19 @@ int main() {
 
     //Binary conversion
     cout << "Testing binary conversion\n----------------\n";
-    cout << "17 -> " << binary.convert(17) << endl;
-    cout << "82 -> " << binary.convert(82) << endl;
-    cout << "45 -> " << binary.convert(45) << endl << endl;
+    cout << "26 -> " << binary.convert(26) << endl;
+    cout << "43 -> " << binary.convert(43) << endl;
+    cout << "45 -> " << binary.convert(57) << endl << endl;
 
     //Octal convertion
     cout << "Testing octal conversion\n----------------\n";
-    cout << "3 -> " << octal.convert(3) << endl;
-    cout << "69 -> " << octal.convert(69) << endl;
-    cout << "24 -> " << octal.convert(24) << endl << endl;
+    cout << "26 -> " << octal.convert(26) << endl;
+    cout << "42 -> " << octal.convert(42) << endl;
+    cout << "57 -> " << octal.convert(57) << endl << endl;
 
     //Hex convertion
     cout << "Testing hex conversion\n----------------\n";
-    cout << "91 -> " << hexa.convert(91) << endl;
-    cout << "56 -> " << hexa.convert(56) << endl;
-    cout << "38 -> " << hexa.convert(38) << endl;
+    cout << "26 -> " << hexa.convert(26) << endl;
+    cout << "47 -> " << hexa.convert(47) << endl;
+    cout << "183 -> " << hexa.convert(183) << endl;
 }
