23fbc73e9966eecb7ef3ffb2750410ebf237242b
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-02-01 16:35:19 -0800

    Modified driver file so that it takes inputs instead

--- BaseProgConvert/main.cpp
+++ BaseProgConvert/main.cpp
@@ -12,21 +12,12 @@ int main() {
     ToOctal octal;
     ToHex hexa;
 
-    //Binary conversion
-    cout << "Testing binary conversion\n----------------\n";
-    cout << "26 -> " << binary.convert(26) << endl;
-    cout << "43 -> " << binary.convert(43) << endl;
-    cout << "45 -> " << binary.convert(57) << endl << endl;
-
-    //Octal convertion
-    cout << "Testing octal conversion\n----------------\n";
-    cout << "26 -> " << octal.convert(26) << endl;
-    cout << "42 -> " << octal.convert(42) << endl;
-    cout << "57 -> " << octal.convert(57) << endl << endl;
-
-    //Hex convertion
-    cout << "Testing hex conversion\n----------------\n";
-    cout << "26 -> " << hexa.convert(26) << endl;
-    cout << "47 -> " << hexa.convert(47) << endl;
-    cout << "183 -> " << hexa.convert(183) << endl;
+    while (true) {
+        int value = 0;
+        cout << "Decimal value: ";
+        cin >> value;
+        cout << "Binary value: " << binary.convert(value) << endl;
+        cout << "Octal value: " << octal.convert(value) << endl;
+        cout << "Hexadecimal value: " << hexa.convert(value) << endl << endl;
+    }
 }
